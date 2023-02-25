25ad7ae40f16957b0153a198e0dfede026bdd6ce
Krunal Patel <krunalkachadia@gmail.com>
2023-02-25 10:24:02 +0530

    .

---
+++ encoder.py
@@ -0,0 +1,20 @@
+
+from sklearn.preprocessing import LabelEncoder
+
+# Convert the string features into numerical features using sklearn LabelEncoder.
+train_features = ["Apple", "Orange", "Potato", "Tomato", "Grape", "Apple", "Tomato", "Grape"]
+test_features = ["Apple", "Orange", "Tomato"]
+le = LabelEncoder()
+
+# START EDIT
+transformed_train = []
+transformed_test = []
+# END EDIT
+
+print(train_features)
+print(test_features)
+print(transformed_train)
+print(transformed_test)
+
+
+
